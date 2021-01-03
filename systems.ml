a6846a55c4fa60faf1e64f75ee55b7bbb1c3a16a
Tireur2cables <nicolas.battaglini@hotmail.fr>
2021-01-03 22:47:41 +0100

    prise en compte des [ et ]
    Problem : 
    
    Solution : 
    
    Note :

--- systems.ml
+++ systems.ml
@@ -15,102 +15,130 @@ type 's system = {
 (** Put here any type and function implementations concerning systems *)
 
 let is_comment line =
-  if (String.length line) > 0 then (
-  String.get line 0 = '#') else false
+  if (String.length line) > 0
+  then line.[0] = '#'
+  else false
 ;;
 
 let is_space line =
-  if (String.length line) = 0 then
-    true else String.get line 0 = '\n';
+  if (String.length line) = 0
+  then true
+  else line.[0] = '\n';
 ;;
 
 let rec interpret_line file block axiom rules inter  =
   try
     let x = input_line file in
-    if is_comment x then (
-      interpret_line file block axiom rules inter;
-    )
-    else (
-      if is_space x then (
-        interpret_line file (block+1) axiom rules inter;
-      )else (
-        if block = 0 then(
-          interpret_line file block (axiom ^ x) rules inter;
-        )else if block = 1 then (
-          interpret_line file block axiom (rules ^ x ^ ":") inter;
-        )
-        else(
-          interpret_line file block axiom rules (inter ^ x ^ ":");
-        )
-      );
-    );
-  with End_of_file -> (
-      (axiom, rules, inter);
-    )
+    if is_comment x
+    then interpret_line file block axiom rules inter
+
+    else if is_space x
+    then interpret_line file (block+1) axiom rules inter
+
+    else if block = 0
+    then interpret_line file block (axiom ^ x) rules inter
+
+    else if block = 1
+    then interpret_line file block axiom (rules ^ x ^ ":") inter
+
+    else interpret_line file block axiom rules (inter ^ x ^ ":")
+
+  with End_of_file -> (axiom, rules, inter)
 ;;
 
-let rec axiom_of_string s =
+let word_of_string s =
+  let rec seq_of_string s res =
+    match s with
+    | "" -> (res, s)
+    | s ->
+       let sub = String.sub s 1 (String.length s - 1) in
+       match s.[0] with
+       | '[' ->
+          begin
+            let (newres, news) = seq_of_string sub [] in
+            match newres with
+            | [] -> seq_of_string news res
+            | w :: [] -> seq_of_string news (res @ [Branch (w)])
+            | _ -> seq_of_string news (res @ [Branch (Seq (newres))])
+          end
+       | ']' -> (res, sub)
+       | c -> seq_of_string sub (res @ [Symb (String.make 1 c)])
+  in
   match s with
-  | "" -> []
-  | s -> Symb (String.make 1 (String.get s 0)) :: (axiom_of_string (String.sub s 1 ((String.length s)-1) ) );;
+  | "" -> failwith "Erreur l'axiome est vide!"
+  | s ->
+     if String.length s = 1 then Symb s
+     else
+       let (res, str) = seq_of_string s [] in
+       Seq res
+;;
 
 let rec string_of_word w =
   match w with
   | Symb s -> s
-  | Seq s -> (match s with
-        | [] -> ""
-        | s :: l -> (string_of_word s) ^ string_of_word (Seq l)
-      )
-  | Branch b -> string_of_word b
+  | Seq s ->
+     begin
+       match s with
+       | [] -> ""
+       | s :: l -> (string_of_word s) ^ string_of_word (Seq l)
+     end
+  | Branch b -> "[" ^ (string_of_word b) ^ "]"
 ;;
 
-let rec word_of_string s =
-  match s with
-  | "" -> Symb ""
-  | s ->  if String.length s = 1 then ( Symb s ) else ( Seq(Symb (String.make 1 (String.get s 0)) :: (word_of_string (String.sub s 1 ((String.length s)-1))) :: []))
-
-let rec create_pair rules_list =
-  match rules_list with
+let rec create_paires list =
+  match list with
   | [] -> []
-  | w :: list -> let res = String.split_on_char ' ' w in
-  	if List.length res = 1 then [] else
-    [(List.nth res 0, List.nth res 1)] @ create_pair list
+  | w :: list ->
+     let res = String.split_on_char ' ' w in
+  	 if List.length res = 1 then [] (* mauvais formattage ou fin de ligne *)
+     else [(List.nth res 0, List.nth res 1)] @ (create_paires list)
+;;
+
+let make_match_function action paires =
+  fun s ->
+  begin
+    match List.assoc_opt s paires with
+    | None -> action s
+    | Some r -> r
+  end
 ;;
 
 let create_rules rules =
   let rules_list = String.split_on_char ':' rules in
-  let rules_pair = create_pair rules_list in
-  function s -> (match List.assoc_opt s rules_pair with
-    | None -> word_of_string s
-    | Some r -> word_of_string r
-    )
+  let rules_paires = create_paires rules_list in
+  let transform (s1, s2) =
+    (s1, word_of_string s2)
+  in
+  let paires = List.map transform rules_paires in
+  make_match_function word_of_string paires
 ;;
 
 let create_interp inter =
-  let rules_list = String.split_on_char ':' inter in
-  let rules_pair = create_pair rules_list in
-  let fun_aux (s1,s2) =
+  let inter_list = String.split_on_char ':' inter in
+  let inter_paires = create_paires inter_list in
+  let transform (s1,s2) =
     match String.get s2 0 with
     | 'L' -> (s1, [Turtle.Line (int_of_string (String.sub s2 1 ((String.length s2)-1)))])
     | 'M' -> (s1, [Turtle.Move (int_of_string (String.sub s2 1 ((String.length s2)-1)))])
     | 'T' -> (s1, [Turtle.Turn (int_of_string (String.sub s2 1 ((String.length s2)-1)))])
-    | _ -> failwith "erreur"
+    | _ -> failwith "Erreur commande de tortue inconnue!"
   in
-  let paires = List.map fun_aux rules_pair in
-  function s ->
-    begin
-      match List.assoc_opt s paires with
-      | None -> failwith "erreur liste pas complete"
-      | Some r -> r
-    end
+  let paires = List.map transform inter_paires in
+  let action s =
+    match s with
+    | "[" -> [Turtle.Store]
+    | "]" -> [Turtle.Restore]
+    | _ -> failwith ("Erreur " ^ s ^ " n'est pas dans la liste des interprétations!")
+  in
+  make_match_function action paires
 ;;
 
 let interpret_file file =
    let canal_in = open_in file in
-   let (axiom, rules, inter) = interpret_line canal_in 0 "" "" "" in
-   let ax = Seq (axiom_of_string axiom) in
-   let rules = create_rules rules in
-   let interp = create_interp inter in
+   let (axiom_str, rules_str, interp_str) = interpret_line canal_in 0 "" "" "" in
+   let axiom = word_of_string axiom_str in
+   let rules = create_rules rules_str in
+   let interp = create_interp interp_str in
    close_in canal_in;
-   {axiom = ax; rules = rules; interp = interp}
+   {axiom = axiom; rules = rules; interp = interp}
 ;;
