a0adfa45693c5c927b0b89e7d4e2eac1c4d17f39
Barrault Victor <victorbarrault@gmail.com>
2021-01-07 16:46:11 +0100

    Calcul de la taille

--- turtle.ml
+++ turtle.ml
@@ -115,12 +115,12 @@ let calc_size t c (hp, vp, hn, vn) =
 
 let exec_command (t: turtle) (c: command) (coefx, coefy) (maxx, maxy) : (turtle) =
   match c with
-    
+
   | Line i -> (* move while drawing by i pixels *)
-     let newx = (float_of_int i) *. (cos ((t.current_pos.a /. 180.) *. pi)) in
+     let newx = ((maxx/.coefx)*.(float_of_int i)) *. (cos ((t.current_pos.a /. 180.) *. pi)) in
 	 let newx = round newx in
      let newx = int_of_float (newx +. t.current_pos.x) in
-     let newy = (float_of_int i) *. (sin ((t.current_pos.a /. 180.) *. pi)) in
+     let newy = ((maxx/.coefx)*.(float_of_int i)) *. (sin ((t.current_pos.a /. 180.) *. pi)) in
 	 let newy = round newy in
      let newy = int_of_float (newy +. t.current_pos.y) in
      lineto (newx) (newy);
